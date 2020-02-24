48c81e99f76daf35d0fe96bb6c8f8e7c88725d66
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2020-02-25 04:18:08 +0600

    Update cpt.php

--- cpt.php
+++ cpt.php
@@ -234,43 +234,6 @@ function eduhub_register_my_cpts_section() {
 	
 	
 	
-	$labels = [
-		"name" => __( "Reservation", "eduhub" ),
-		"singular_name" => __( "Reservation", "eduhub" ),
-		
-        
-	];
-
-	$args = [
-		"label" => __( "Reservation", "eduhub" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => false,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"delete_with_user" => false,
-		"show_in_rest" => true,
-		"rest_base" => "",
-		"rest_controller_class" => "WP_REST_Posts_Controller",
-		"has_archive" => true,
-		"show_in_menu" => true,
-		"show_in_nav_menus" => false,
-		"delete_with_user" => false,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => true,
-		"rewrite" => [ "slug" => "reservation", "with_front" => true ],
-		"query_var" => true,
-		"menu_position" => 10,
-		"menu_icon" => "dashicons-book-alt",
-		"supports" => [ "title"],
-        "taxonomies"=>array('category'),
-	];
-
-	register_post_type( "reservation", $args );
-	
-	
 		$labels = [
 		"name" => __( "Video", "eduhub" ),
 		"singular_name" => __( "Video", "eduhub" ),
